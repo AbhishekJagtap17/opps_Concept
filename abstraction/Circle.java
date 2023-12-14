c0c7850bde5e4e6e112f60dfb1df719b5cd5ebaa
AbhishekJagtap17 <135544422+AbhishekJagtap17@users.noreply.github.com>
2023-12-14 23:18:17 +0530

    Add files via upload

---
+++ abstraction/Circle.java
@@ -0,0 +1,14 @@
+package abstraction;
+
+public class Circle extends GraphicObject {
+@Override
+void draw() {	
+	System.out.println("Drawing a Circle");
+	
+}
+@Override
+	void resize() {
+	System.out.println("Resizing a Circle");
+		
+	}
+}
