a797bd0dc01c3c4c9b0d6ba8c3d206c7f1d7f386
AbhishekJagtap17 <135544422+AbhishekJagtap17@users.noreply.github.com>
2023-12-14 23:21:13 +0530

    Add files via upload

---
+++ Encapsulation02/B.java
@@ -0,0 +1,15 @@
+package Encapsulation02;
+
+import Encapsulation.A;
+
+public class B {
+    public static void main(String[] args) {
+     A a=new A();
+        System.out.println(a.a);
+    //CtE    System.out.println(a.b);
+    //CTE    System.out.println(a.c);
+     //CTE   System.out.println(a.d);
+
+    }
+
+}
